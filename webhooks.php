244643076e51a474982882e707c79e72192d19d9
Khunsornchai <khunsornchai@yahoo.com>
2018-10-27 12:28:52 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'dY/y4ctZDxf4WR7BTgvCxaFWUCxq1fbyF/4Q6BIVAW1XFbqlPA2H9shsCy76QJoIf6+kFmTONW0FzvXVsN4z5qViMtrZgwd56fMZpRujrHVfYOyj11NeQothrCskV9LtwO1hEeBA/Wjyre86H2JUMgdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');

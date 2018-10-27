ac67d6c61e62fe66fcadf903bf8daef1c59bcacc
Khunsornchai <khunsornchai@yahoo.com>
2018-10-27 13:17:44 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,9 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'dY/y4ctZDxf4WR7BTgvCxaFWUCxq1fbyF/4Q6BIVAW1XFbqlPA2H9shsCy76QJoIf6+kFmTONW0FzvXVsN4z5qViMtrZgwd56fMZpRujrHVfYOyj11NeQothrCskV9LtwO1hEeBA/Wjyre86H2JUMgdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = '8198ede1d11a55f3df2ba26e1fbf2889';
 
 $pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
 
